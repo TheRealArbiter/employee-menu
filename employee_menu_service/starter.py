4ce34ad3ad1db9f5a08098dc96e45208a0bf6ce4
Luis Guzman <luisguzman.dev@gmail.com>
2022-03-05 22:37:38 -0600

    se integran los modelos para los menús, comidas y comensales

---
+++ employee_menu_service/starter.py
@@ -0,0 +1,22 @@
+from django.views.generic import View
+from django.http import JsonResponse
+from employee_menu_service.settings import json_settings
+
+settings = json_settings()
+
+class Starter(View):
+    """
+    Métdo para consultar si el servicio se encuentra activo.
+    """
+    def get(self, request):
+        if settings['USE_HTTPS']:
+            api_url = "https://"
+        else:
+            api_url = "http://"
+        # server_port = request.environ.get('SERVER_PORT', None)
+        # if server_port:
+        #     api_url += f"{request.tenant.domain_url}:{server_port}"
+        # else:
+        #     api_url += f"{request.tenant.domain_url}"
+        json_object = {'info': "API Service Ready", 'api-documentation': f'{api_url}/api/doc/'}
+        return JsonResponse(json_object)
--- employee_menu_service/urls.py
+++ employee_menu_service/urls.py
@@ -1,21 +1,27 @@
-"""employee_menu_service URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .starter import Starter
+from employee_menu_service.apps.apidoc.config import schema_view
 
 urlpatterns = [
+    path('', Starter.as_view(), name="starter"),
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
+    # API configuration.
+    path('api/doc/(?P<format>\.json|\.yaml)/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # Own Django Apps.
+    # path('person/', include('people.apps.person.urls')),
+    # path('setup/', include('people.apps.setup.urls')),
+    # path('business/', include('people.apps.business.urls')),
+    # path('noticenter/', include('people.apps.noticenter.urls')),
+    # path('payroll/', include('people.apps.payroll.urls')),
+    # path('intranet/', include('people.apps.intranet.urls')),
+    # path('fiscal/', include('people.apps.fiscal.urls')),
 ]
+
+if settings.DEBUG:  # Adding statics only if debug = True
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
